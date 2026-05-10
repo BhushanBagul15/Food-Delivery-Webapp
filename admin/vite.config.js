e654eb86d6e332dba1e9ed40df47431867d2e909
Bhushan Bagul <bhushan.17391@sakec.ac.in>
2026-05-11 01:05:51 +0530

    solved admin panel error

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-=======
   base: '/admin/',
->>>>>>> 4238abde62fe7bf465a73fe5959bc54d7d36aa4b
 })
