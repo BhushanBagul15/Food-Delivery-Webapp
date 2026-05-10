5b9b533b7fc9e57a34f76a50098924aff39db617
BhushanBagul15 <bhushan.17391@sakec.ac.in>
2026-05-10 17:01:58 +0530

    Update frontend URL in placeOrder function

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -8,7 +8,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 //placing user order from frontend
 const placeOrder = async (req,res) => {
 
-        const frontend_url = req.body.origin || "https://food-delivery-webapp-8j7z-git-main-bhushanbagul15s-projects.vercel.app/";
+        const frontend_url = req.body.origin || "https://food-delivery-webapp-8j7z.vercel.app/";
 
         try {
             const newOrder = new orderModel({
